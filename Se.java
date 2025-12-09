87bd2dad01eb0e1cad151c379fe9b2c5a833a754
Llriglana <llriglana@MacBook-Air-Llriglana.local>
2025-12-09 20:33:08 +0300

    last

--- Se.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-class Se {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String pop = scan.nextLine();
-        System.out.println(pop.endsWith("burg"));
-    }
-}
