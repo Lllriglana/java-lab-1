23303e5cbe48f194952ae5b1dcca48f9a588dd24
Llriglana <llriglana@MacBook-Air-Llriglana.local>
2025-12-10 00:12:29 +0300

    финал...

--- Main.java
+++ Main.java
@@ -1,52 +1,75 @@
 public class Main {
-    private static double formula(double x) {
-        double part1 = Math.pow(2.0 * x, 3) / 2.0;
-        double atanPart = Math.atan((x - 1.0) / 28.0);
-        double xPowPart = Math.pow(x, 1.0 / 3.0) + 1.0;
-        double fraction = part1 / (xPowPart / atanPart);
-        double leftBlock = Math.pow(fraction, 3);
-        double expInner = Math.pow(x / 2.0, 3);
-        double middleBlock = Math.cos(Math.pow(Math.E, expInner));
-        double acosInner = 0.1 * (x - 1.0) / 28.0;
-        double acosPart = Math.acos(acosInner);
-        double expAcos = Math.pow(Math.E, acosPart);
-        double rightBlock = 0.5 - Math.asin(1.0 / expAcos);
-        return leftBlock * middleBlock * rightBlock;
+    private static double Third(double x) {
+        // числитель главной дроби
+        double Chisl = Math.atan((x - 4.0) / 18.0);
+        double Chisl1 = Math.tan(Chisl / 2.0);
+        // возведение в степень числителя
+        double chislitel = Math.pow(Chisl1, 2.0 * x);
+
+        // знаменатель
+        double logor = Math.log(Math.abs(x));
+        double stepen_znam = Math.pow(logor, 2.0 * Math.exp(x));
+        double znam = 1.0 - Math.tan(stepen_znam);
+        // дробь
+        double drob = chislitel / znam;
+
+        // первая степень дроби
+        double stepen_drobi1 = Math.pow(2.0 * Math.pow(x, x), Chisl1);
+
+        // вторая степень дроби
+        double s = x / (x - 0.5) - 1.0;
+        double p = 0.5 * s;
+        double i = Math.pow(p, Math.cbrt(x));
+        double stepen_drobi2 = (3.0 / 4.0) * (Math.PI + i);
+
+        return drob * stepen_drobi1 * stepen_drobi2;
     }
 
     public static void main(String[] args) {
-
-        long[] v = new long[] {4, 6, 8, 10, 12, 14, 16};
-        double[] x = new double[15];
+        int[] w = new int[16];
+        int chislo = 18;
+        for (int i = 0; i < w.length; i++) {
+            w[i] = chislo--;
+        }
+        float[] x = new float[12];
         for (int j = 0; j < x.length; j++) {
-            x[j] = -15 + Math.random() * 28;  // случайные числа
+            x[j] = (float) (-13.0 + Math.random() * 18.0);
         }
 
-        double[][] d = new double[7][15];
+        double[][] f = new double[16][12];
 
-        for (int i = 0; i < v.length; i++) {
-            if (v[i] == 4) {
-                for (int j = 0; j < x.length; j++) {
-                    double X = x[j];
-                    d[i][j] = Math.tan(Math.tan(Math.log(Math.abs(X))));
+        for (int i = 0; i < w.length; i++) {
+            for (int j = 0; j < x.length; j++) {
+                double X = x[j]; // работаем в double
+
+                if (w[i] == 4) {
+                    // основание
+                    double osnov = 0.5 * (Math.cos(X) - 0.5);
+                    // степень
+                    double koren_v_step = Math.pow(osnov, Math.cbrt(X));
+                    double step1 = Math.log(Math.abs(X));
+                    // степень арксинуса
+                    double step2 = Math.pow(step1, 2.0 * Math.asin((X - 4.0) / 18.0));
+
+                    f[i][j] = Math.pow(koren_v_step, step2);
+
+                } else if (w[i] == 3 || w[i] == 6 || w[i] == 7 || w[i] == 8
+                        || w[i] == 9 || w[i] == 10 || w[i] == 12 || w[i] == 17) {
+
+                    double second = Math.atan(1.0 / Math.pow(Math.E, Math.abs(X))) - 1.0;
+                    f[i][j] = Math.pow(second, 3.0);
+
+                } else {
+                    f[i][j] = Third(X);
                 }
             }
-            else if (v[i] == 12 || v[i] == 14 || v[i] == 16) {
-               for (int j = 0; j < x.length; j++ )  {
-                   double X = x[j];
-                   d[i][j] = Math.tan((Math.sin(X) - Math.PI) / 4.0);
-               }
+        }
+
+        for (int i = 0; i < f.length; i++) {
+            for (int j = 0; j < f[i].length; j++) {
+                System.out.printf("%8.2f ", f[i][j]);
             }
-            else {
-                for (int j = 0; j < x.length; j++) {
-                    double X = x[j];
-                    d[i][j] = formula(X);
-                }
-            }
-            for (int j = 0; j < d[i].length; j++) {
-                System.out.printf("%10.4f ", d[i][j]);;  // четыре знака после запятой
-            }
-            System.out.println(); // перенос строки
+            System.out.println();
         }
     }
 }
